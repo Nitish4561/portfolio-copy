f1cbd7285ef5d9c3b9fc34b378c46ac8f24b59e8
Nitish Kalra <11410771+Nitish4561@users.noreply.github.com>
2025-12-26 23:03:33 +0530

    test

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -19,5 +19,9 @@ export function Button({ variant = 'primary', className, href, ...props }) {
     <Link href={href} className={className} {...props} />
   ) : (
     <button className={className} {...props} />
+    <button className={className} {...props} />
+    <button className={className} {...props} />
+    <button className={className} {...props} />
+    
   )
 }
