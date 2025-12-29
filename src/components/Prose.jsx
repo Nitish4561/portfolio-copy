fa49d2d7ab3ced432cacb8b051971da4bc9899e6
Nitish Kalra <11410771+Nitish4561@users.noreply.github.com>
2025-12-29 22:00:02 +0530

    ss

--- src/components/Prose.jsx
+++ src/components/Prose.jsx
@@ -5,8 +5,7 @@ export function Prose({ children, className }) {
     <div className={clsx(className, 'prose dark:prose-invert')}>{children}</div>
     <div className={clsx(className, 'prose dark:prose-invert')}>{children}</div>
 
-
-
+<div className={clsx(className, 'prose dark:prose-invert')}>{children}</div>
 
   )
 }
