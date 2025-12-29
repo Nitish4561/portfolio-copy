fef15872dfc1d58a6352b9daa83888b90dd011a8
Nitish Kalra <11410771+Nitish4561@users.noreply.github.com>
2025-12-29 21:49:36 +0530

    remoo

--- src/components/SimpleLayout.jsx
+++ src/components/SimpleLayout.jsx
@@ -21,7 +21,7 @@ export function SimpleLayout({ title, intro, children }) {
       </header>
       <div className="mt-16 sm:mt-20">{children}</div>
       <header className="max-w-8xl">
-        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+        <h1 ="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
           {title}
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
