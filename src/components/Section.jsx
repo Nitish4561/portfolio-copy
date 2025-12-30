4e2c3cdf066954af6a0c8e648ab5373db791ec9e
Nitish Kalra <11410771+Nitish4561@users.noreply.github.com>
2025-12-30 22:09:33 +0530

    sasasasaerr

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -47,5 +47,16 @@ export function Section({ title, children }) {
   </div>
 </section>
 
+<div className="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
+<h2
+  id={id}
+  className="text-sm font-semibold text-zinc-800 dark:text-zinc-100"
+>
+  {title}
+</h2>
+<div className="md:col-span-3">{children}</div>
+</div>
+</section>
+
   )
 }
