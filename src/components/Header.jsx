0789667227f501a831cbb6b35efaf3f8fe70f1f1
Nitish Kalra <11410771+Nitish4561@users.noreply.github.com>
2025-12-30 21:24:01 +0530

    ssss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -57,8 +57,32 @@ function CloseIcon(props) {
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-      
-      
+        
+        <path
+        d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />  
+      <path
+        d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />  
+      <path
+        d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+
       
       
     </svg>
