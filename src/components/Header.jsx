dd0ba642e32a2b82e6fc7d91d6bbf50b74ed0ee3
Nitish Kalra <11410771+Nitish4561@users.noreply.github.com>
2025-12-30 22:02:42 +0530

    sasasasasasa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -96,7 +96,15 @@ path
         stroke="currentColor"
         strokeWidth="1.5"
       
-      
+      />  
+      <path
+        d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
 
       
       
